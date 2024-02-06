bf3b348cef1a511a38070586c337a44aaae4080d
scarmonger <psikomania@yahoo.com>
2024-02-06 21:23:22 +0700

    fix diagnostic error

--- config/nvim/lua/custom/plugins/plugins.lua
+++ config/nvim/lua/custom/plugins/plugins.lua
@@ -75,26 +75,26 @@ return {
 			symbol = "│",
 			options = { try_as_border = true },
 		},
-		init = function()
-			vim.api.nvim_create_autocmd("FileType", {
-				pattern = {
-					"help",
-					"alpha",
-					"dashboard",
-					"neo-tree",
-					"Trouble",
-					"trouble",
-					"lazy",
-					"mason",
-					"notify",
-					"toggleterm",
-					"lazyterm",
-				},
-				callback = function()
-					vim.b.miniindentscope_disable = true
-				end,
-			})
-		end,
+		-- init = function()
+		-- 	vim.api.nvim_create_autocmd("FileType", {
+		-- 		pattern = {
+		-- 			"help",
+		-- 			"alpha",
+		-- 			"dashboard",
+		-- 			"neo-tree",
+		-- 			"Trouble",
+		-- 			"trouble",
+		-- 			"lazy",
+		-- 			"mason",
+		-- 			"notify",
+		-- 			"toggleterm",
+		-- 			"lazyterm",
+		-- 		},
+		-- 		callback = function()
+		-- 			vim.b.miniindentscope_disable = true
+		-- 		end,
+		-- 	})
+		-- end,
 	},
 	{
 		"echasnovski/mini.bufremove",
